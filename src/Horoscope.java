b3dc0f201de04be068ee67a91b34a50ba3bb00a5
Paige-Steuber <64327507+Paige-Steuber@users.noreply.github.com>
2022-03-27 14:31:28 -0500

    can now assign signs

--- src/Horoscope.java
+++ src/Horoscope.java
@@ -7,6 +7,102 @@ public class Horoscope {
 	}
 	
 	private String assignSign(int month, int day){	
+		if(month == 3) {
+			if(day < 21) {
+				sign = "Pisces";
+			}
+			else {
+				sign = "Aries";
+			}
+		}
+		if(month == 4) {
+			if(day < 20) {
+				sign = "Aries";
+			}
+			else {
+				sign = "Taurus";
+			}
+		}
+		if(month == 5) {
+			if(day < 21) {
+				sign = "Taurus";
+			}
+			else {
+				sign = "Gemini";
+			}
+		}
+		if(month == 6) {
+			if(day < 21) {
+				sign = "Gemini";
+			}
+			else {
+				sign = "Cancer";
+			}
+		}
+		if(month == 7) {
+			if(day < 23) {
+				sign = "Cancer";
+			}
+			else {
+				sign = "Leo";
+			}
+		}
+		if(month == 8) {
+			if(day < 23) {
+				sign = "Leo";
+			}
+			else {
+				sign = "Virgo";
+			}
+		}
+		if(month == 9) {
+			if(day < 23) {
+				sign = "Virgo";
+			}
+			else {
+				sign = "Libra";
+			}
+		}
+		if(month == 10) {
+			if(day < 23) {
+				sign = "Libra";
+			}
+			else {
+				sign = "Scorpio";
+			}
+		}
+		if(month == 11) {
+			if(day < 22) {
+				sign = "Scorpio";
+			}
+			else {
+				sign = "Sagittarius";
+			}
+		}
+		if(month == 12) {
+			if(day < 22) {
+				sign = "Sagittarius";
+			}
+			else {
+				sign = "Capricorn";
+			}
+		}
+		if(month == 1) {
+			if(day < 20) {
+				sign = "Capricorn";
+			}
+			else {
+				sign = "Aquarius";
+			}
+		}
+		if(month == 2) {
+			if(day < 19) {
+				sign = "Aquarius";
+			}
+			else {
+				sign = "Pisces";
+			}
+		}
 		
 		return null;
 	}
@@ -16,7 +112,7 @@ public class Horoscope {
 	}
 	
 	public String getSign() {
-		return null;
+		return this.sign;
 	}
 
 }
