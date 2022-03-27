ed75cd497227d1823d2ede47aa00ca6c9cd4d462
agstull <annag@DESKTOP-66VQEK5.localdomain>
2022-03-26 20:15:46 -0500

    displays interface before user picks activity

--- src/Welcome/WelcomeInterface.java
+++ src/Welcome/WelcomeInterface.java
@@ -1,24 +1,35 @@
 package Welcome;
+
 import java.util.Scanner;
 
 public class WelcomeInterface {
 
 	public static void main(String[] args) {
-		String name = promptUserForName();
+		Scanner scanner = new Scanner(System.in);
+		String name = promptUserForName(scanner);
 		displayWelcomeInterface(name);
+		String activity = promptUserForActivity(scanner);
+		scanner.close();
 	}
 
-	public static String promptUserForName() {
+	public static String promptUserForName(Scanner scanner) {
 		System.out.println("Hi! What's your name?");
-		Scanner scanner  = new Scanner(System.in);
 		String input = "";
 		while (input == "") {
 			input = scanner.nextLine();
 		}
-		scanner.close();
 		return input;
 	}
-	
+
+	public static String promptUserForActivity(Scanner scanner) {
+		String input = "";
+		while (!input.toLowerCase().equals("horoscope") && !input.toLowerCase().equals("compatibility calculator")
+				&& !input.toLowerCase().equals("quizzes")) {
+			input = scanner.nextLine();
+		}
+		return input;
+	}
+
 	public static void displayWelcomeInterface(String name) {
 		System.out.println("Welcome, " + name + "!");
 		System.out.println("What would you like to do first?");
