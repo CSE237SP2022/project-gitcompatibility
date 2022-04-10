ab54b21db28719acb0046caba900b1cdcab604de
megjoplin <70993976+megjoplin@users.noreply.github.com>
2022-04-10 17:21:15 -0500

    figuring out tests

--- src/tests/QuizResultTests.java
+++ src/tests/QuizResultTests.java
@@ -13,10 +13,9 @@ class QuizResultTests {
 		QuizInput quiz = new QuizInput();
 		quiz.yesCounter = 4; 
 		quiz.noCounter = 2; 
-		int option;
 		
 		quiz.yesAndNo();
-		option = quiz.option;
+		int option = quiz.option;
 		
 		assertEquals(1, option); 
 	}
@@ -26,10 +25,9 @@ class QuizResultTests {
 		QuizInput quiz = new QuizInput();
 		quiz.yesCounter = 2; 
 		quiz.noCounter = 4; 
-		int option;
-		
+
 		quiz.yesAndNo();
-		option = quiz.option;
+		int option = quiz.option;
 		
 		assertEquals(2, option); 
 	}
@@ -39,10 +37,9 @@ class QuizResultTests {
 		QuizInput quiz = new QuizInput();
 		quiz.yesCounter = 2; 
 		quiz.noCounter = 2; 
-		int option;
 		
 		quiz.yesAndNo();
-		option = quiz.option;
+		int option = quiz.option;
 		
 		assertEquals(3, option);
 	}
