702b49c904f39e199564a7d3a605497b02afff9d
Paige-Steuber <a.steuber@wustl.edu>
2022-04-12 14:04:04 -0500

    adding random Daily Horoscope

--- src/tests/horoscopeTests.java
+++ src/tests/horoscopeTests.java
@@ -8,10 +8,15 @@ import org.junit.jupiter.api.Test;
 
 import Horoscope.Horoscope;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Scanner;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.LinkedList;
@@ -47,6 +52,41 @@ class horoscopeTests {
 	@Test
 	void testGetCompatibilityNotCompatible() {
 		assertFalse(horoscopeTest.getCompatibility(7, 11));
+	}
+	
+	@Test
+	void testRandomAdjectivesList() {
+		int numberOfwords = horoscopeTest.getAdjectivesList().size();
+		int realNumberOfWords = 34;
+		assertEquals(realNumberOfWords, numberOfwords);
+	}
+	
+	@Test
+	void testRandomAdjective() {
+		String word = horoscopeTest.getRandomWordFromList(horoscopeTest.getAdjectivesList());
+		
+		File myFile = new File("src/adjectives.txt");
+		try {
+			
+			Scanner myReader = new Scanner(myFile);
+			String line = myReader.nextLine();
+
+			List<String> words = new ArrayList<String>();
+			while( myReader.hasNextLine()) {		    	
+				words.add(line);
+				line = myReader.nextLine();;
+			}
+			assertTrue(words.contains(word));
+
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
 	}
 
 }
